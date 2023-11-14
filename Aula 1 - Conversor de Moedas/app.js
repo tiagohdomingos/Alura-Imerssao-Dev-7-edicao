e300255d38eb1a141ba5345d5d82a91f198e75ef
TIAGO HENRIQUE DOMINGOS <73835817+tiagohdomingos@users.noreply.github.com>
2023-11-14 14:04:51 -0300

    Desafio aula 1

---
+++ Aula 1 - Conversor de Moedas/app.js
@@ -0,0 +1,24 @@
+var valorEmDolar = 64;
+var cotacaoDolarDia = 5.32;
+var valorEmReal = valorEmDolar * cotacaoDolarDia;
+valorEmReal = valorEmReal.toFixed(2);
+
+var nomeUsuario = "Tiago Domingos";
+var mensagemInicio = "Olá, ";
+var mensagemFinal = " o valor que vc pediu de ";
+
+var quantosAnosLuz = 4000;
+var velocidadeDaLuz = 299792458;
+var segundosAnoJuliano = 31557600;
+var anoLuzm = velocidadeDaLuz * segundosAnoJuliano;
+anoLuzm = anoLuzm.toFixed(2);
+alert(
+  mensagemInicio +
+    nomeUsuario +
+    mensagemFinal +
+    quantosAnosLuz +
+    " ly" +
+    " é de " +
+    anoLuzm +
+    " m."
+);

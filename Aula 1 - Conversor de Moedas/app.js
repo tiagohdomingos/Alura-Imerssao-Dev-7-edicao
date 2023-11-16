37314b7414f16adcb7234df4c6d66efc3663a05b
TIAGO HENRIQUE DOMINGOS <73835817+tiagohdomingos@users.noreply.github.com>
2023-11-16 10:38:11 -0300

    inclusao JS da aula

--- Aula 1 - Conversor de Moedas/app.js
+++ Aula 1 - Conversor de Moedas/app.js
@@ -7,7 +7,11 @@ var nomeUsuario = "Tiago Domingos";
 var mensagemInicio = "Olá, ";
 var mensagemFinal = " o valor que vc pediu de ";
 
+<<<<<<< Updated upstream
 var quantosAnosLuz = 4000;
+=======
+var quantosAnosLuz = 220;
+>>>>>>> Stashed changes
 var velocidadeDaLuz = 299792458;
 var segundosAnoJuliano = 31557600;
 var anoLuzm = velocidadeDaLuz * segundosAnoJuliano;
